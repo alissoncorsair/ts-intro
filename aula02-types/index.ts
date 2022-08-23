c5c9728e8d8128674d1ace7e0c0c3c80754772b9
alisson <alisson@microsistec.com.br>
2022-08-23 13:20:11 -0300

    05 - 9:00

---
+++ aula02-types/index.ts
@@ -0,0 +1,21 @@
+/** boolean */
+let isOpen: boolean;
+isOpen = true;
+// isOpen = "false";  dá erro
+
+/** string */
+let message: string;
+message = "hello world";
+
+/** number (int, float, hex, binary) */
+let total: number; 
+total = 0xff0;
+
+/** array (type[]) */
+let items: string[];
+items = ["a", "b", "c"];
+
+let values: Array<number>;
+values = [1, 2, 3];
+
+
