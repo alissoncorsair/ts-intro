2ab688f4a0e957c61526b9574b6b529de90b8984
Alisson <alissoncorsair@hotmail.com>
2022-08-24 00:28:04 -0300

    05. types 13:00

--- aula02-types/dist/index.js
+++ aula02-types/dist/index.js
@@ -1,46 +1,35 @@
+"use strict";
 /** boolean */
-let isOpen: boolean;
+let isOpen;
 isOpen = true;
 // isOpen = "false";  dá erro
-
 /** string */
-let message: string;
+let message;
 message = "hello world";
-
 /** number (int, float, hex, binary) */
-let total: number; 
+let total;
 total = 0xff0;
-
 /** array (type[]) */
-let items: string[];
+let items;
 items = ["a", "b", "c"];
-
-let values: Array<number>;
+let values;
 values = [1, 2, 3];
-
-let words: Array<string>
+let words;
 words = ["foo", "boo", "bah"];
-
-
 // tuple <- a gente sabe o número de elementos dentro desse array e o tipo de cada elemento
-let title: [number, string, boolean];
+let title;
 title = [1, "alguma string", false];
-
 // enum <- utilizado principalmente para quando queremos criar conjuntos de chave:valor
-enum Colors {
-    white = '#fff',
-    black = '#000'
-}
-
+var Colors;
+(function (Colors) {
+    Colors["white"] = "#fff";
+    Colors["black"] = "#000";
+})(Colors || (Colors = {}));
 // any (qualquer coisa) <- nao eh legal usar esse aqui
-let coisa: any;
+let coisa;
 coisa = "qualquer coisa" || true || 2;
-
-
 //void
-const sayHi =  ():void => {
+const sayHi = () => {
     console.log("Hi");
-}
-
+};
 sayHi();
-
